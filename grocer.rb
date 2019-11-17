37b37c85fb6e1781f734e663bd1d662542a90a5f
Finn Thye <piqueen@gmail.com>
2019-11-17 07:53:18 -0700

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
   row_index = 0
   name_hash = {}
@@ -18,13 +20,34 @@ end
 #[{:item=>"DOG FOOD"}, {:item=>"WINE"}, {:item=>"STRYCHNINE"}]
 
 def consolidate_cart(cart)
+  binding.pry
+  flat_cart = cart.flatten
+  #binding.pry
   consolidated = []
+  row_index = 0
 
-  pp cart
+  while row_index < cart.length do
+    inner_index = 0
+
+    while inner_index < cart[row_index].length do
+      item_name = cart[row_index][inner_index][:item]
+
+      if consolidated #hash.has_value?(item_name)
+        #item exists so increment :count += 1
+      else
+        #item not in [] yet so transfer hash plus :count = 1
+      end
+
+      inner_index += 1
+    end
+
+    row_index += 1
+  end
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  consolidated
 end
 # [{:item=>"TEMPEH", :price=>3.0, :clearance=>true},
 #   {:item=>"PEANUTBUTTER", :price=>3.0, :clearance=>true},
