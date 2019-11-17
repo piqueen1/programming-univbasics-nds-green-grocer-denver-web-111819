ff3783bc6c76ba98312f0ec5ee4be37646a2fd50
Finn Thye <piqueen@gmail.com>
2019-11-17 07:04:42 -0700

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,37 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  row_index = 0
+  name_hash = {}
+
+  while row_index < collection.length do
+    if collection[row_index][:item] === name
+      name_hash = collection[row_index]
+      break
+    else
+      name_hash = nil
+    end
+
+    row_index += 1
+  end
+
+  name_hash
 end
+#[{:item=>"DOG FOOD"}, {:item=>"WINE"}, {:item=>"STRYCHNINE"}]
 
 def consolidate_cart(cart)
+  consolidated = []
+
+  pp cart
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 end
+# [{:item=>"TEMPEH", :price=>3.0, :clearance=>true},
+#   {:item=>"PEANUTBUTTER", :price=>3.0, :clearance=>true},
+#   {:item=>"ALMONDS", :price=>9.0, :clearance=>false}]
+#  [{:item=>"AVOCADO", :price=>3.0, :clearance=>true},
+#   {:item=>"AVOCADO", :price=>3.0, :clearance=>true},
+#   {:item=>"KALE", :price=>3.0, :clearance=>false}]
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
